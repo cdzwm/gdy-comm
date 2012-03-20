8f5fe04fbf1089eca594a0d6f4e7fed09999afbb
canw <nodejs@live.cn>
2012-03-20 13:09:28 +0800

    add newRespMessage

--- message.js
+++ message.js
@@ -13,7 +13,7 @@ var msg_re_begin = "GDY_MSG_BEGIN\{("
 
 // parseMessae function
 // ignore non-message data
-module.exports.parseMessage = function(msg_data, mq){
+exports.parseMessage = function(msg_data, mq){
 	var msg_text, last_pos = 0;
 	try{
 		while((msg_text = msg_re.exec(msg_data)) != null){
@@ -26,26 +26,31 @@ module.exports.parseMessage = function(msg_data, mq){
 	return last_pos;
 }
 
-module.exports.new = function(cmd){
+exports.new = function(cmd){
 	var message = {cmd: cmd};
 	return message;
 }
 
-module.exports.pack = function(msg){
+exports.pack = function(msg){
 		return msg_begin + JSON.stringify(msg) + msg_end;
 }
 
-module.exports.msgBegin = function(){
+exports.msgBegin = function(){
 	return msg_begin;
 }
-module.exports.msgEnd = function(){
+exports.msgEnd = function(){
 	return msg_end;
 }
 
-module.exports.msgReBegin = function(){
-	return module.exports.msgBegin().replace(/([\[\]$()*+.?\\^{|}])/g,'\\$1');
+exports.msgReBegin = function(){
+	return exports.msgBegin().replace(/([\[\]$()*+.?\\^{|}])/g,'\\$1');
 }
 
-module.exports.msgReEnd = function(){
-	return module.exports.msgEnd();
+exports.msgReEnd = function(){
+	return exports.msgEnd();
+}
+
+exports.newRespMessage = function(msg){
+	var m = exports.new(msg.cmd + "_resp");
+	return m;
 }
